d35e530b912b52a8dbb789632c9680ca821d6dfe
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-11 15:25:02 +0300

    Add some corrections

--- interview_preps/01_concatenation_of_types.js
+++ interview_preps/01_concatenation_of_types.js
@@ -12,18 +12,18 @@ console.log(undefined + 42) // NaN
 //comparison:
 console.log('comparison-----------------');
 console.log(2 == '2') //true compiler will compare all as unified type by default: this is string and in 
-console.log(2 === '2') // here compiler will take into consideration current type of each object
-console.log(undefined == null) // true, because - underfined - smth exists
-console.log('0' == false) //true but here)) in this exceptional situation we have true, because compiler will try to compare as numeric types, because here we have boolean value and compiler will cast it to numerid 
+console.log(2 === '2') // false, because here compiler will take into consideration current type of each object
+console.log(undefined == null) // true, because - underfined - smth exists ????????
+console.log('0' == false) //true but here)) in this exceptional situation we have true, because compiler will try to compare as numeric types, because here we have boolean value and compiler will cast it to numeric 
 console.log('0' == 0) //true
 console.log(0 == 0) //true
 console.log('-----------------');
-console.log(false == '') //false
-console.log(false == []) //true
-console.log(false == {}) //true
-console.log('' == 0) //true
-console.log('' == []) //true
-console.log('' == {}) //true
-console.log(0 == []) //true
-console.log(0 == {}) //true
-console.log(0 == null) //true
+console.log(false == '') //false //true
+console.log(false == []) //true //true true array is not an object?
+console.log(false == {}) //true //false there is smth as object as true that exists = 1
+console.log('' == 0) //false //true, because compiler think that '' = 0
+console.log('' == []) //false //true
+console.log('' == {}) //false //false
+console.log(0 == []) //false //true array is not an object?
+console.log(0 == {}) //false //false// there is smth as object as true that exists = 1
+console.log(0 == null) //true //false
