3dc640aa9585c05605a55a4eeb5166b83f87b159
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-11 17:12:33 +0300

    Add hosting samples

---
+++ interview_preps/04_hosting.js
@@ -0,0 +1,18 @@
+var i
+console.log(i)
+i = 42
+console.log(i)
+
+console.log("---------")
+
+console.log(num)
+let num = 42
+console.log()
+
+console.log("---------")
+
+// Function Expression & Function Declaration
+console.log(square(25))
+function square(num) {
+	return num ** 2
+}
