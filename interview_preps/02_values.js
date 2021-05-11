2260bc3c3d06551b797f5842a65c0e2b892ebd3a
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-11 18:45:51 +0300

    Add closures logic

--- interview_preps/02_values.js
+++ interview_preps/02_values.js
@@ -4,16 +4,17 @@ b++
 console.log("a", a)
 console.log("b", b)
 
-console.log("---------")
+console.log("1---------")
 // here we copy link, but not an object/cell with data
-let a = [1, 2, 3]
-let b = a
-b.push(4)
+
+// let a = [1, 2, 3]
+// let b = a
+// b.push(4)
 
 console.log("a", a)
 console.log("b", b)
 
-console.log("---------")
+console.log("2---------")
 // here we copy link, but not an object/cell with data
 let a = [1, 2, 3]
 let b = a.concat() // here we forbid
---
+++ interview_preps/06_closures.js
@@ -0,0 +1,47 @@
+console.log("1 st sample-------------")
+function sayHelloToPerson(name) {
+	const message = "Hello" + name
+	return function () {
+		console.log(message)
+	}
+}
+
+result = sayHelloToPerson("Irina")
+result()
+
+console.log("2 nd sample-------------")
+function createFrameworkManager() {
+	const fw = ["Angular", "React"]
+	return {
+		print: function () {
+			console.log(fw.join(" "))
+		},
+		add: function (framework) {
+			fw.push(framework)
+		},
+	}
+}
+
+const manager = createFrameworkManager()
+manager.print()
+manager.add("VueJS")
+manager.print()
+
+// setTimeout
+
+const fib = [1, 2, 3, 5, 8, 13]
+//let is available only in a declared scope
+for (let i = 0; i < fib.length; i++) {
+	setTimeout(function () {
+		console.log(`fib[${i}] = ${fib[i]}`)
+	}, 1500)
+}
+
+// const fib1 = [1, 2, 3, 5, 8, 13]
+// for (var i = 0; i < fib1.length; i++) {
+// 	;(function (j) {
+// 		setTimeout(function () {
+// 			console.log(`fib1[${i}] = ${fib1[i]}`)
+// 		}, 1500)
+// 	})(i)
+// }
