edc768deb805a68f1c932b4f4e4d8ee08b70f468
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-11 07:51:43 +0300

    Change file name

---
+++ interview_preps/01_casting_types.js
@@ -0,0 +1,13 @@
+let language = "JavaScript"
+if ((language = "JavaScript")) {
+	console.log("The best language is:", language)
+}
+console.log(Boolean("")) //false
+console.log(Boolean("Hello")) //true
+console.log(Boolean(" ")) //true
+console.log(Boolean("0")) //true
+console.log(Boolean(0)) //false
+console.log(Boolean(null)) //false
+console.log(Boolean([])) //true, видимо, считается, что это пустой объект
+console.log(Boolean({})) //true, видимо, считается, что это пустой объект
+console.log(Boolean(function () {})) //true, видимо, считается, что это пустой объект
