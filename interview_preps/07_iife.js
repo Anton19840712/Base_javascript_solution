a84041a918cde9cc4b974bd8b230d3dbdca4ed81
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-12 15:03:23 +0300

    Add some comments, change some code strings for better understanding

--- interview_preps/07_iife.js
+++ interview_preps/07_iife.js
@@ -1,14 +1,16 @@
 //Immediate Invoked Function Expression
 let result = []
-// for (var i = 0; i < 5; i++) {
-// 	result.push(function () {
-// 		console.log(i)
-// 	})
-// }
+for (var i = 0; i < 5; i++) {
+	result.push(function () {
+		console.log(i)
+	})
+}
 
-// result[2]()
-// result[4]()
+result[2]()
+result[4]()
 
+// here for each element you run function
+let result = []
 for (var i = 0; i < 5; i++) {
 	;(function () {
 		var j = i
