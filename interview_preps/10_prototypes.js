a2e6abd28c4c8b7ee3fc780f9768c9eb83820329
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-12 19:55:44 +0300

    Add code for description of the concept of prototype

---
+++ interview_preps/10_prototypes.js
@@ -0,0 +1,50 @@
+//__proto__
+// Object.getPrototypeOf()
+
+function Cat(name, color) {
+	this.name = name
+	this.color = color
+}
+
+Cat.prototype.voice = function () {
+	console.log(`Cat ${this.name} says myay`)
+}
+
+const cat = new Cat("Cat", "white")
+
+console.log(Cat.protoype)
+console.log(cat)
+console.log(Cat.voice)
+console.log((Cat.voice = "loudly"))
+console.log(cat.__proto__ === Cat.prototype)
+console.log(cat.constructor)
+
+//=================
+
+function Person() {}
+Person.prototype.legs = 2
+Person.prototype.skin = "white"
+
+const person = new Person()
+person.name = "Antonio"
+// person.skin = 'white'
+
+console.log("skin" in person)
+console.log(person.legs)
+console.log(person.name)
+console.log(person.hasOwnProperty("name")) //true
+console.log(person.hasOwnProperty("skin")) //false, because there is no elements in person(only person.name = 'Antonio')
+
+//Inheritance using prototypes
+
+const proto = { year: 2019, level: "good" }
+const myYear = Object.create(proto) // this object is a prototype, because we used create
+console.log(myYear) //{}
+console.log(myYear.year)
+// console.log(myYear.year = 2019)
+console.log(myYear.hasOwnProperty("year"))
+console.log(myYear.hasOwnProperty("level"))
+console.log(myYear.__proto__ === proto)
+proto.year = 2200
+console.log(myYear.year)
+console.log(proto.year)
