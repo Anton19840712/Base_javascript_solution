4928e99f0c0c24077cb5c79a5ef66348699cc652
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-13 17:11:04 +0300

    Add files to understand the features of the debug process

---
+++ debug_how/app.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+	// 1....... First you are bind to the html elements from your html page
+	// we are using querySelector method to bind with elements from UI
+
+	const num1 = document.querySelector("#num1")
+	const num2 = document.querySelector("#num2")
+	const addBtn = document.querySelector("#add")
+	const subBtn = document.querySelector("#sub")
+	const output = document.querySelector("#output")
+
+	// 2....... Then you should write methods here to manipulate with gotten elements
+
+	function getInputValues() {
+		const value1 = parseInt(num1.value) //we can parse string, we got in a previous code part
+		const value2 = +num2.value // or in a more short variant we can write plus
+		return [value1, value2] // You may return two values as array
+	}
+
+	function addHandler() {
+		//debugger - this is like a breakpoint on chrome debugger
+		const values = getInputValues()
+		// console.log(values)
+		const result = values[0] + values[1]
+		displayResult(result)
+	}
+
+	function subHandler() {
+		const values = getInputValues()
+		const result = values[0] - values[1]
+		displayResult(result)
+	}
+
+	function displayResult(result) {
+		//this function will show the result of calculations
+		output.closest(".card").style.display = "block"
+		output.innerHTML = `Result = ${result}` // transfer result of calcualtions here using innerHTML
+		console.trace()
+	}
+	// console.log('Test', addBtn)
+
+	// 3....... Here we add listeners part of coding
+	addBtn.addEventListener("click", addHandler) // It listens, if
+	subBtn.addEventListener("click", subHandler)
+})
---
+++ debug_how/more-debug.js
@@ -0,0 +1,9 @@
+const people = [
+	{ id: 1, name: "Antonio", age: 20 },
+	{ id: 2, name: "Irina", age: 19 },
+	{ id: 3, name: "Anna", age: 22 },
+	{ id: 4, name: "Olga", age: 23 },
+	{ id: 5, name: "Inna", age: 25 },
+]
+console.log(people)
+console.table(people)
