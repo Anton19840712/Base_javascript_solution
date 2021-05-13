f1922bdced7207a2f00e116beb0a7d1292a1f920
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-13 17:23:14 +0300

    Add comments to async sample.

--- interview_preps/11_async.js
+++ interview_preps/11_async.js
@@ -1,7 +1,9 @@
+// 1 You make several arrow methods
 const first = () => console.log("First const")
 const second = () => console.log("Second const")
 const third = () => console.log("Third const")
 
+// 2 Then you call them and one of them call inside timeout method...
 first()
 //second()
 setTimeout(second, 0) //callbackqueue and there event loops
