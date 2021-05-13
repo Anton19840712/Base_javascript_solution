bc3ca56022fc77d5f1261309e6aa82da0dd92d80
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-13 19:53:14 +0300

    Add new feature sample to work with strings.

---
+++ new_features_2021/01_strings.js
@@ -0,0 +1,5 @@
+//Quokka use for current file
+const stringSeq =
+	"JS is the best language in the world. Let's learn JS together."
+console.log(stringSeq.replace("JS", "Java"))
+console.log(stringSeq.replaceAll("JS", "Python"))
