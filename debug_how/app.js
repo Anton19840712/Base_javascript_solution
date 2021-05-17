e2244b65f409c378c3dbab4eddd31a18068c5d24
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-05-17 15:23:43 +0300

    Reordered methods sequence for a better percetption

--- debug_how/app.js
+++ debug_how/app.js
@@ -10,12 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// 2....... Then you should write methods here to manipulate with gotten elements
 
-	function getInputValues() {
-		const value1 = parseInt(num1.value) //we can parse string, we got in a previous code part
-		const value2 = +num2.value // or in a more short variant we can write plus
-		return [value1, value2] // You may return two values as array
-	}
-
 	function addHandler() {
 		//debugger - this is like a breakpoint on chrome debugger
 		const values = getInputValues()
@@ -24,10 +18,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		displayResult(result)
 	}
 
-	function subHandler() {
-		const values = getInputValues()
-		const result = values[0] - values[1]
-		displayResult(result)
+	function getInputValues() {
+		const value1 = parseInt(num1.value) //we can parse string, we got in a previous code part
+		const value2 = +num2.value // or in a more short variant we can write plus
+		return [value1, value2] // You may return two values as array
 	}
 
 	function displayResult(result) {
@@ -36,6 +30,14 @@ document.addEventListener("DOMContentLoaded", () => {
 		output.innerHTML = `Result = ${result}` // transfer result of calcualtions here using innerHTML
 		console.trace()
 	}
+
+	function subHandler() {
+		const values = getInputValues()
+		const result = values[0] - values[1]
+		displayResult(result)
+	}
+
+
 	// console.log('Test', addBtn)
 
 	// 3....... Here we add listeners part of coding
