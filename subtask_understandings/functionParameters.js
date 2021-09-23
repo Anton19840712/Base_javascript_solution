4db1dec6eb634eccff5495cfffac13e453997aa9
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-09-23 10:30:24 +0300

    [-]  some minifications to work this code better

--- subtask_understandings/functionParameters.js
+++ subtask_understandings/functionParameters.js
@@ -1,15 +1,14 @@
 function ReturnBaseData(namev, surname) {
 	console.log("here1")
-	var elements = namev + surname
-	return elements
+	return namev + surname
 }
 
 function PushInitialData() {
 	console.log("here2")
 	var namev = 24
 	var surname = 76
-	sum = ReturnBaseData(namev, surname)
+	var sum = ReturnBaseData(namev, surname)
 	console.log(sum)
 }
 
-//PushInitialData()
+PushInitialData()
