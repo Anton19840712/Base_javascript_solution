eb444c3b493b05647ac013f5347b5962a6ef2684
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-09-24 18:42:12 +0300

    several cahnges witn params and dicts.

--- subtask_understandings/functionParameters.js
+++ subtask_understandings/functionParameters.js
@@ -1,10 +1,8 @@
-function ReturnBaseData(namev, surname) {
-	console.log("here1")
-	return namev + surname
+function ReturnBaseData(x, y) {
+	return x + y
 }
 
 function PushInitialData() {
-	console.log("here2")
 	var namev = 24
 	var surname = 76
 	var sum = ReturnBaseData(namev, surname)
