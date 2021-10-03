daf7d60cbd2cf76d0ef5ccfb0815a7fc56a84390
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-03 13:51:33 +0300

    Add map sample usage.

---
+++ common_aspects/mapSampleUsage.js
@@ -0,0 +1,16 @@
+const users = [{ name: "Elena" }, { name: "Alex" }, { name: "Irina" }]
+
+const visits = new Map()
+
+visits
+	.set(users[0], new Date())
+	.set(users[1], new Date(new Date().getTime() + 1000 * 60))
+	.set(users[2], new Date(new Date().getTime() + 5000 * 60))
+
+//console.log(visits)
+
+function lastVisit(user) {
+	return visits.get(user)
+}
+
+console.log(lastVisit(users[2]))
