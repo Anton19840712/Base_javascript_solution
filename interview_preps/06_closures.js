e0255473916cf625bacd7c0d85b85497ecac7df8
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-10-16 14:59:48 +0300

    Some notes for closures.

--- interview_preps/06_closures.js
+++ interview_preps/06_closures.js
@@ -1,45 +1,59 @@
-console.log("1 st sample-------------")
-function sayHelloToPerson(name) {
-	const message = "Hello" + name
-	return function () {
-		console.log(message)
+var result = doCount()
+console.log(result()) // заметь, что здесь ты пишешь result(), а не result
+console.log(result())
+console.log(result())
+
+function doCount() {
+	var counter = 0 // везде counter пишешь, да и все.
+	function countMe() {
+		counter = counter + 1
+		return counter
 	}
+	return countMe // здесь ты возвращаешь функцию
 }
 
-result = sayHelloToPerson("Irina")
-result()
+// console.log("1 st sample-------------")
+// function sayHelloToPerson(name) {
+// 	const message = "Hello" + name
+// 	return function () {
+// 		console.log(message)
+// 	}
+// }
 
-console.log("2 nd sample-------------")
-//Seems to me that is as a class oblect in C#
-//Because we can address the methods of this function)) sounds like heresy)
-//
-function createFrameworkManager() {
-	const fw = ["Angular", "React"]
-	return {
-		print: function () {
-			console.log(fw.join(" "))
-		},
-		// I don't know why I need this)
-		add: function (framework) {
-			fw.push(framework)
-		},
-	}
-}
-const manager = createFrameworkManager()
-manager.print()
+// result = sayHelloToPerson("Irina")
+// result()
 
-manager.add("VueJS")
-manager.print()
+// console.log("2 nd sample-------------")
+// //Seems to me that is as a class oblect in C#
+// //Because we can address the methods of this function)) sounds like heresy)
+// //
+// function createFrameworkManager() {
+// 	const fw = ["Angular", "React"]
+// 	return {
+// 		print: function () {
+// 			console.log(fw.join(" "))
+// 		},
+// 		// I don't know why I need this)
+// 		add: function (framework) {
+// 			fw.push(framework)
+// 		},
+// 	}
+// }
+// const manager = createFrameworkManager()
+// manager.print()
 
-// setTimeout
+// manager.add("VueJS")
+// manager.print()
 
-const fib = [1, 2, 3, 5, 8, 13]
-//let is available only in a declared scope
-for (let i = 0; i < fib.length; i++) {
-	setTimeout(function () {
-		console.log(`fib[${i}] = ${fib[i]}`)
-	}, 1500)
-}
+// // setTimeout
+
+// const fib = [1, 2, 3, 5, 8, 13]
+// //let is available only in a declared scope
+// for (let i = 0; i < fib.length; i++) {
+// 	setTimeout(function () {
+// 		console.log(`fib[${i}] = ${fib[i]}`)
+// 	}, 1500)
+// }
 
 // const fib1 = [1, 2, 3, 5, 8, 13]
 // for (var i = 0; i < fib1.length; i++) {
